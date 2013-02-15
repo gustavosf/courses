2ec3819fe3e9a81e7f952baff2829bb629577823
Gustavo Seganfredo <gustavosf@gmail.com>
2013-02-15 20:14:30 -0200

    Ok, coverage 100% on controllers and models

--- edx/BerkeleyX/CS169.1x/homework-4/spec/controllers/movies_controller_spec.rb
+++ edx/BerkeleyX/CS169.1x/homework-4/spec/controllers/movies_controller_spec.rb
@@ -14,13 +14,28 @@ describe MoviesController do
     end
     it 'should select the Find Similar template for rendering' do
       Movie.stub(:similar_to).and_return(@similar)
-      post :similar, {:id => 1}
+      get :similar, {:id => 1}
       response.should render_template('similar')
     end
     it 'should make similar movies available to that template' do
       Movie.stub(:similar_to).and_return(@similar)
-      post :similar, {:id => 1}
+      get :similar, {:id => 1}
       assigns(:movies).should == Movie.find_all_by_director('George Lucas')
+    end
+    it 'should return an error if movie director is undefined' do
+      Movie.stub(:similar_to).and_return(@similar)
+      get :similar, {:id => 3}
+      response.should redirect_to(movies_path)
+    end
+  end
+
+  describe 'delete movie' do
+    it 'should select the movie to delete' do
+      @movie = Movie.find(1)
+      Movie.should_receive(:find).with('1').and_return(@movie)
+      @movie.should_receive(:destroy)
+
+      delete :destroy, :id => @movie
     end
   end
 
--- edx/BerkeleyX/CS169.1x/homework-4/spec/models/movie_spec.rb
+++ edx/BerkeleyX/CS169.1x/homework-4/spec/models/movie_spec.rb
@@ -2,10 +2,18 @@ require 'spec_helper'
 
 describe Movie do
   fixtures :movies
+
   describe 'find similar movies by director' do
-    it 'should call find movie by director with movie director' do
+    it 'should find movies with the same director as one given' do
       Movie.should_receive(:find_all_by_director)
-      Movie.first.find_similar
+      Movie.similar_to(Movie.find_by_director 'George Lucas')
     end
   end
+
+  describe 'list movie ratings' do
+    it 'should list all available movie ratings' do
+      Movie.all_ratings.should == %w(G PG PG-13 NC-17 R)
+    end
+  end
+
 end
