ca2d1c3375a15ab3fd599d18b02a48cd3552b413
Gustavo Seganfredo <gustavosf@gmail.com>
2013-02-19 12:00:58 -0300

    Done isWordGuessed

--- edx/MITx/6.00x/week-3/part2/ps3_hangman.py
+++ edx/MITx/6.00x/week-3/part2/ps3_hangman.py
@@ -1,5 +1,5 @@
 # 6.00 Problem Set 3
-# 
+#
 # Hangman game
 #
 
@@ -17,7 +17,7 @@ WORDLIST_FILENAME = "words.txt"
 def loadWords():
     """
     Returns a list of valid words. Words are strings of lowercase letters.
-    
+
     Depending on the size of the word list, this function may
     take a while to finish.
     """
@@ -53,7 +53,10 @@ def isWordGuessed(secretWord, lettersGuessed):
     returns: boolean, True if all the letters of secretWord are in lettersGuessed;
       False otherwise
     '''
-    # FILL IN YOUR CODE HERE...
+    for letter in secretWord:
+        if not (letter in lettersGuessed):
+            return False
+    return True
 
 
 
@@ -75,7 +78,7 @@ def getAvailableLetters(lettersGuessed):
       yet been guessed.
     '''
     # FILL IN YOUR CODE HERE...
-    
+
 
 def hangman(secretWord):
     '''
@@ -83,16 +86,16 @@ def hangman(secretWord):
 
     Starts up an interactive game of Hangman.
 
-    * At the start of the game, let the user know how many 
+    * At the start of the game, let the user know how many
       letters the secretWord contains.
 
     * Ask the user to supply one guess (i.e. letter) per round.
 
-    * The user should receive feedback immediately after each guess 
+    * The user should receive feedback immediately after each guess
       about whether their guess appears in the computers word.
 
-    * After each round, you should also display to the user the 
-      partially guessed word so far, as well as letters that the 
+    * After each round, you should also display to the user the
+      partially guessed word so far, as well as letters that the
       user has not yet guessed.
 
     Follows the other limitations detailed in the problem write-up.
