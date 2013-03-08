546b367bee5de41367e9e9fc8df807d9f50c0fe1
Gustavo Seganfredo <gustavosf@gmail.com>
2013-03-08 08:33:15 -0300

    Part 8 done

--- edx/MITx/6.00x/week-4/ps4b.py
+++ edx/MITx/6.00x/week-4/ps4b.py
@@ -9,7 +9,7 @@ import time
 #
 def compChooseWord(hand, wordList, n):
     """
-    Given a hand and a wordList, find the word that gives 
+    Given a hand and a wordList, find the word that gives
     the maximum value score, and return it.
 
     This word should be calculated by considering all the words
@@ -26,7 +26,7 @@ def compChooseWord(hand, wordList, n):
     # Create a new variable to store the maximum score seen so far (initially 0)
     max_score = 0
 
-    # Create a new variable to store the best word seen so far (initially None)  
+    # Create a new variable to store the best word seen so far (initially None)
     best_word_seen = None
 
     # For each word in the wordList
@@ -51,24 +51,36 @@ def compChooseWord(hand, wordList, n):
 def compPlayHand(hand, wordList, n):
     """
     Allows the computer to play the given hand, following the same procedure
-    as playHand, except instead of the user choosing a word, the computer 
+    as playHand, except instead of the user choosing a word, the computer
     chooses it.
 
     1) The hand is displayed.
     2) The computer chooses a word.
-    3) After every valid word: the word and the score for that word is 
-    displayed, the remaining letters in the hand are displayed, and the 
+    3) After every valid word: the word and the score for that word is
+    displayed, the remaining letters in the hand are displayed, and the
     computer chooses another word.
     4)  The sum of the word scores is displayed when the hand finishes.
     5)  The hand finishes when the computer has exhausted its possible
     choices (i.e. compChooseWord returns None).
- 
+
     hand: dictionary (string -> int)
     wordList: list (string)
     n: integer (HAND_SIZE; i.e., hand size required for additional points)
     """
-    # TO DO ... <-- Remove this comment when you code this function
-    
+    total_score = 0
+
+    while calculateHandlen(hand) > 0:
+        displayHand(hand)
+        word = compChooseWord(hand, wordList, n)
+        if (word == None):
+            break
+        else:
+            score = getWordScore(word, n)
+            total_score += score
+            print '"%s" earned %d points. Total: %d points\n' % (word, score, total_score)
+            hand = updateHand(hand, word)
+    print 'Total score: %d points.' % total_score
+
 #
 # Problem #8: Playing a game
 #
@@ -76,7 +88,7 @@ def compPlayHand(hand, wordList, n):
 def playGame(wordList):
     """
     Allow the user to play an arbitrary number of hands.
- 
+
     1) Asks the user to input 'n' or 'r' or 'e'.
         * If the user inputs 'e', immediately exit the game.
         * If the user inputs anything that's not 'n', 'r', or 'e', keep asking them again.
@@ -87,10 +99,10 @@ def playGame(wordList):
     3) Switch functionality based on the above choices:
         * If the user inputted 'n', play a new (random) hand.
         * Else, if the user inputted 'r', play the last hand again.
-      
+
         * If the user inputted 'u', let the user play the game
           with the selected hand, using playHand.
-        * If the user inputted 'c', let the computer play the 
+        * If the user inputted 'c', let the computer play the
           game with the selected hand, using compPlayHand.
 
     4) After the computer or user has played the hand, repeat from step 1
@@ -100,7 +112,7 @@ def playGame(wordList):
     # TO DO... <-- Remove this comment when you code this function
     print "playGame not yet implemented." # <-- Remove this when you code this function
 
-        
+
 #
 # Build data structures used for entire session and play game
 #
