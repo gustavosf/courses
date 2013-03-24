ae25b4579556c45c46e1d03c94622a12baf1bea5
Gustavo Seganfredo <gustavosf@gmail.com>
2013-03-24 11:23:47 -0300

    P7 done

---
+++ edx/MITx/6.00x/midterm/problem-7.py
@@ -0,0 +1,50 @@
+import re
+
+# part of problem-5
+def generateForm(story, listOfAdjs, listOfNouns, listOfVerbs):
+    """
+    story: a string containing sentences
+    listOfAdjs: a list of valid adjectives
+    listOfNouns: a list of valid nouns
+    listOfVerbs: a list of valid verbs
+
+    For each word in story that is in one of the lists,
+    * replace it with the string '[ADJ]' if the word is in listOfAdjs
+    * replace it with the string '[VERB]' if the word is in listOfVerbs
+    * replace it with the string '[NOUN]' if the word is in listOfNouns
+
+    returns: string, A Mad-Libs form of the story.
+    """
+    for word in re.findall(r"[\w]+", story):
+        if (word in listOfNouns): story = story.replace(word, '[NOUN]')
+        if (word in listOfAdjs): story = story.replace(word, '[ADJ]')
+        if (word in listOfVerbs): story = story.replace(word, '[VERB]')
+    return story
+
+# part of problem-7
+def generateTemplates(madLibsForm):
+    """
+    madLibsForm: string, in a Mad-Lib form. See output of `generateForm`
+
+    returns: a list of '[ADJ]', '[VERB]', and '[NOUN]' strings, in
+    the order they appear in the madLibsForm.
+    """
+    return re.findall(r'(\[ADJ\]|\[NOUN\]|\[VERB\])', madLibsForm)
+
+if __name__ == '__main__':
+    # Test cases for problem-5 (generateForm) and problem-7 (generateTemplates)
+    story = 'The ravenous zombies started attacking yesterday'
+    listOfAdjs = ['ravenous']
+    listOfNouns = ['zombies', 'humans', 'yesterday']
+    listOfVerbs = ['attacking', 'attacks']
+    form = generateForm(story, listOfAdjs, listOfNouns, listOfVerbs)
+    assert form == 'The [ADJ] [NOUN] started [VERB] [NOUN]'
+    assert generateTemplates(form) == ['[ADJ]', '[NOUN]', '[VERB]', '[NOUN]']
+
+    story = 'At the creepy thrift store I found a pair of plaid pants that looked like something your grandpa might wear'
+    listOfAdjs = ['creepy', 'plaid']
+    listOfNouns = ['store', 'pants', 'something', 'grandpa']
+    listOfVerbs = ['found', 'looked']
+    form = generateForm(story, listOfAdjs, listOfNouns, listOfVerbs)
+    assert form == 'At the [ADJ] thrift [NOUN] I [VERB] a pair of [ADJ] [NOUN] that [VERB] like [NOUN] your [NOUN] might wear'
+    assert generateTemplates(form) == ['[ADJ]', '[NOUN]', '[VERB]', '[ADJ]', '[NOUN]', '[VERB]', '[NOUN]', '[NOUN]']
