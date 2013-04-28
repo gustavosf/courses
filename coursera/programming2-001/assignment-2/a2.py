9bc630516d3c9a3a9c1a81397e86fa4c3579f6cc
Gustavo Seganfredo <gustavosf@gmail.com>
2013-04-28 20:07:14 -0300

    Written code for the maze

--- coursera/programming2-001/assignment-2/a2.py
+++ coursera/programming2-001/assignment-2/a2.py
@@ -44,11 +44,43 @@ class Rat:
 	def eat_sprout(self):
 		self.num_sprouts_eaten += 1
 	def __str__(self):
-		print "%s at (%d, %d) ate %d sprouts." % \
+		return "%s at (%d, %d) ate %d sprouts." % \
 			(self.symbol, self.row, self.column, self.num_sprouts_eaten)
 
 
 class Maze:
 	""" A 2D maze. """
-
-	# Write your Maze methods here.
+	def __init__(self, maze, rat_1, rat_2):
+		self.maze = maze
+		self.rat_1 = rat_1
+		self.rat_2 = rat_2
+		self.num_sprouts_left = sum(x.count(SPROUT) for x in maze)
+	def is_wall(self, row, col):
+		return maze[row][col] == WALL
+	def get_character(self, row, col):
+		if self.rat_1.row == row and self.rat_1.col == col:
+			return RAT_1_CHAR
+		elif self.rat_2.row == row and self.rat_2.col == col:
+			return RAT_2_CHAR
+		else:
+			return maze[row][col]
+	def move(self, rat, row, col):
+		pos_x = rat.row + row
+		pos_y = rat.col + col
+		if (not self.is_wall(row, col)):
+			rat.set_location(pos_x, pos_y)
+			if (self.maze[pos_x, pos_y] == SPROUT):
+				rat.eat_sprout
+				self.num_sprouts_eaten -= 1
+				self.maze[pos_x, pos_y] = HALL
+			return True
+		return False
+	def __str__(self):
+		output = ''
+		maze = self.maze
+		maze[self.rat_1.x][self.rat_1.y] = RAT_1_CHAR
+		maze[self.rat_2.x][self.rat_2.y] = RAT_2_CHAR
+		for line in maze:
+			output += ''.join(maze) + '\n'
+		output += self.rat_1 + '\n' + self.rat_2,
+		return output
